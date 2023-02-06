f7f9eba5a6ee7557681eb8a14c1326073f430d9d
Kristoffer Kofoed Rødvei <rodvei@hotmail.com>
2023-02-06 01:57:07 +0100

    check blob access

--- dags/preparation_dag.py
+++ dags/preparation_dag.py
@@ -1,5 +1,6 @@
 import datetime
-
+import requests
+from google.cloud import storage
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
@@ -7,23 +8,43 @@ default_args = {
     "owner": "Kristoffer",
     "retries": 5,
     "retry_delay": datetime.timedelta(minutes=5),
-    "start_date": datetime.datetime(2023, 2, 1),
+    "start_date": datetime.datetime(2023, 2, 4),
 }
+
+#brights_bucket_1/preparation_test_folder
 
 
 def run(**kwargs):
-    # This function will be called by airflow, specified by python_callable in teh PythonOperator
-    # print(kwargs)
-    print('This is Kristoffer first dag!!')
+    # API Inspiration: 
+    # https://rapidapi.com/collection/list-of-free-apis
+    # https://mixedanalytics.com/blog/list-actually-free-open-no-auth-needed-apis/
+    bucket_name = 'brights_bucket_1'
+    lon = 10.757933
+    lat = 59.911491
+    url = f"https://www.7timer.info/bin/astro.php?lon={lon}&lat={lat}&ac=0&unit=metric&output=json&tzshift=0"
+    res = requests.get(url)
+    res_data = res.json()
+    data_date = res_data['init'][:-2]
+    data = []
+    header = ['timepoint', 'cloudcover']
+    for data_i in res_data['dataseries']:
+        data.append([data_i['timepoint'], data_i['cloudcover']])
+    storage_client = storage.Client()
+    blobs = storage_client.list_blobs(bucket_name)
+    print('get all blobs names:')
+    for blob in blobs:
+        print(blob.name)
+
+
 
 with DAG(
-    dag_id="kristoffer_dag_1",
+    dag_id="preparation_dag",
     description="This is our first test dag",
     default_args=default_args,
     schedule_interval="@daily", #None, @hourly, @weekly, @monthly, @yearly,...
 ) as dag:
 
     run_python_task = PythonOperator(
-        task_id="test_kristoffers_first_task", # This controls what your task name is in the airflow UI 
+        task_id="test_preparation_first_task", # This controls what your task name is in the airflow UI 
         python_callable=run # This is the function that airflow will run 
     )
