edd33f3c17b9df846acff4f00c54790dcb2127b0
Fehmmi Aliti <fehmmialiti@gmail.com>
2023-02-06 13:56:05 +0100

    created a csv file

--- dags/fehmmis_dag.py
+++ dags/fehmmis_dag.py
@@ -2,18 +2,21 @@ import requests
 import http.client, urllib.parse
 import datetime
 import json
+import csv
+import os
+from google.cloud import storage
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 apikey = "acf08f2517abc3baa8e1608b66fcb52e"
 
 default_args = {
-    "owner": "",
+    "owner": "Fehmmi",
     "retries": 1,
     "retry_delay": datetime.timedelta(minutes=5),
     "start_date": datetime.datetime(2023, 1, 1),
 }
-print("hello")
+
 
 def api_call():
     params = {
@@ -25,7 +28,37 @@ def api_call():
 
     response = requests.get("http://api.mediastack.com/v1/news", params=params).json()
     
-    print(response)
+    return response
+
+def transform_data():
+    """getting out relevant information from json and then writing a csv file
+    """
+    api_connection = api_call()
+
+    bucket_name = 'brights_bucket_1'
+    blob_name = 'GB_news.csv'
+
+    my_dict = []
+    header = ['data', 'author', 'source','category']
+
+    for data in api_connection['data']:
+        author = data['author']
+        source = data['source']
+        category = data['category']
+        url = data['url']
+        my_dict.append({'author': author, 'source': source, 'category': category, 'url': url})
+    # print(my_dict)   
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name) 
+
+    blob = bucket.blob(os.path.join('preparation_test_folder/fehmmi', blob_name))
+    with blob.open("w") as f:
+        writer = csv.DictWriter(f, fieldnames=header, lineterminator="\n")
+        writer.writeheader()
+        writer.writerows(data)
+   
+    
+
     
 with DAG(
     "fehmmi-dag",
@@ -35,7 +68,7 @@ with DAG(
 
     run_python_task = PythonOperator(
         task_id="api_call", # This controls what your task name is in the airflow UI 
-        python_callable=api_call # This is the function that airflow will run 
+        python_callable=transform_data # This is the function that airflow will run 
     )
 
 
