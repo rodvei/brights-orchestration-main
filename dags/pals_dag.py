13d741b6b0e511f75f9da9488a7fb4aafab31db9
Pål Renolen <paal.renolen@gmail.com>
2023-02-06 15:04:03 +0100

    planeter og sånn (':

--- dags/pals_dag.py
+++ dags/pals_dag.py
@@ -13,23 +13,31 @@ default_args = {
     "start_date": datetime.datetime(2023, 2, 4),
 }
 
-def get_planets():
+def get_planets(date):
     bucket_name = 'brights_bucket_1'
-    blob_name = 'uranus_planet.csv'
-    url = f'https://api.api-ninjas.com/v1/planets?name=uranus'
-    payload = requests.get(url, payload = requests.get(url, headers={'X-API-Key': 'IfBN/09mgUEHE4M+UdDYkw==VojvtoSTfSBFpOug'}))
-    payload_data = payload.json()
+    blob_name = f'{date}_planet_list.csv'
+    planets = ['mercury','venus','earth','mars','jupiter','saturn','uranus','neptune']
     planet_list = []
-    for rows in payload_data:
-        planet_list.append(rows)
+    for planet in planets:
+        url = f'https://api.api-ninjas.com/v1/planets?name={planet}'
+        payload = requests.get(url, headers={'X-API-Key': 'IfBN/09mgUEHE4M+UdDYkw==VojvtoSTfSBFpOug'})
+        payload_data = payload.json()   
+        planet_list.append(payload_data)
+        for rows in payload_data:
+            planet_list.append(rows)
     client = storage.Client()
     bucket = client.bucket(bucket_name)
     blob = bucket.blob(os.path.join(f'preparation_test_folder/pal', blob_name))
-    with blob.open("w") as f:
-        writer = csv.DictWriter(f, lineterminator="\n")
-        writer.writerows(planet_list)
 
-    print(planet_list)
+    with blob.open("w") as f:
+        writer = csv.DictWriter(f, fieldnames=planet_list[0][0], lineterminator="\n")
+        writer.writeheader()
+        for i in range(len(planet_list)):
+            writer.writerows(planet_list[i])
+
+def run(**kwargs):
+    run_date = kwargs['ds']
+    get_planets(run_date)
 
 with DAG(
     "pals_first_dag",
@@ -37,6 +45,6 @@ with DAG(
     schedule_interval="@daily",
 ) as dag:
     run_planet_task = PythonOperator(
-        task_id = "show_uranus",
-        python_callable = get_planets
+        task_id = "planet_list",
+        python_callable = run
     )
