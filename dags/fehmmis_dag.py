fd90d31c86172a61668547af097cb17390bede4d
Fehmmi Aliti <fehmmialiti@gmail.com>
2023-02-06 10:37:11 +0100

    fehmmis-tag-

---
+++ dags/fehmmis_dag.py
@@ -0,0 +1,14 @@
+import datetime
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+default_args = {
+    "owner": "Ozzyz",
+    "retries": 1,
+    "retry_delay": datetime.timedelta(minutes=5),
+    "start_date": datetime.datetime(2022, 8, 21),
+}
+
+
+
