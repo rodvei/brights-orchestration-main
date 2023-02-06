8aed1387893ab96e729b6055a6532bd6a5f676da
Mats Berg <mats.k.berg@gmail.com>
2023-02-06 11:06:13 +0100

    ferdig

--- dags/mats_dag.py
+++ dags/mats_dag.py
@@ -19,6 +19,6 @@ with DAG(
 ) as dag:
 
     run_crying = PythonOperator(
-        task_id="øl grining", # This controls what your task name is in the airflow UI 
+        task_id="øl_grining", # This controls what your task name is in the airflow UI 
         python_callable=cry # This is the function that airflow will run 
     )
