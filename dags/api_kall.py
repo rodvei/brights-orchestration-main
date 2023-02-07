570bb5072a1cb2cabcf06b1ac90d1834080f313c
IMM <ingrid.morch@gmail.com>
2023-02-07 15:52:29 +0100

    Forgot name change again 2

--- dags/api_kall.py
+++ dags/api_kall.py
@@ -109,7 +109,7 @@ def load(**kwargs):
         print(blob.name)
 
 with DAG(
-    dag_id="dad_kanye_exchange_6",
+    dag_id="dad_kanye_exchange_7",
     description="Dad tells a joke and Kanye answers with a statement that makes sense to him",
     default_args=default_args,
     schedule_interval="@daily", #None, @hourly, @weekly, @monthly, @yearly,...
